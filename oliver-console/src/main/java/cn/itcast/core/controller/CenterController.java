c0479c86674d8d229a7aaa2f1106de71db9f9d33
oliver-baolin <33144128+oliver-baolin@users.noreply.github.com>
2017-11-04 15:37:56 +0800

    提交后台基础页面

--- oliver-console/src/main/java/cn/itcast/core/controller/CenterController.java
+++ oliver-console/src/main/java/cn/itcast/core/controller/CenterController.java
@@ -1,15 +1,12 @@
 package cn.itcast.core.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import cn.itcast.core.bean.TestTb;
-import cn.itcast.core.service.TestTbService;
-
 /**
  * 后台管理中心
+ * 
  * @author Zhang
  *
  */
@@ -17,23 +14,51 @@ import cn.itcast.core.service.TestTbService;
 @RequestMapping("/center")
 public class CenterController {
 
-	@Autowired
-	private TestTbService testTbService;
-	
 	/**
 	 * 测试类
+	 * 
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(value="/index.do")
-	public String test(Model model) {
-		
-		TestTb t = new TestTb();
-		t.setName("oliver");
-		t.setAge(12);
-		testTbService.testTb(t);
-		
+	@RequestMapping(value = "/index.do")
+	public String index(Model model) {
 		return "index";
 	}
-	
+
+	// 头部
+	@RequestMapping(value = "/top.do")
+	public String top(Model model) {
+		return "top";
+	}
+
+	// 身体
+	@RequestMapping(value = "/main.do")
+	public String main(Model model) {
+		return "main";
+	}
+
+	// 身体--左
+	@RequestMapping(value = "/left.do")
+	public String left(Model model) {
+		return "left";
+	}
+
+	// 身体--右
+	@RequestMapping(value = "/right.do")
+	public String right(Model model) {
+		return "right";
+	}
+
+	// 商品--身体
+	@RequestMapping(value = "/frame/product_main.do")
+	public String productMain(Model model) {
+		return "frame/product_main";
+	}
+
+	// 商品--身体
+	@RequestMapping(value = "/frame/product_left.do")
+	public String productLeft(Model model) {
+		return "frame/product_left";
+	}
+
 }
