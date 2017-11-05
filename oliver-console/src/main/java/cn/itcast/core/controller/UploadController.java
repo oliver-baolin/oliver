c8a553280359a666d86191e6f87dae301b62242a
oliver-baolin <33144128+oliver-baolin@users.noreply.github.com>
2017-11-05 19:11:56 +0800

    加入fast分布式文件使用

--- oliver-console/src/main/java/cn/itcast/core/controller/UploadController.java
+++ oliver-console/src/main/java/cn/itcast/core/controller/UploadController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import cn.itcast.common.web.Constants;
 import cn.itcast.core.service.product.UploadService;
 
 /**
@@ -28,15 +29,14 @@ public class UploadController {
 	public void uploadPic(@RequestParam(required = false) MultipartFile pic,
 			HttpServletResponse response) throws IOException{
 		
-//		String path = uploadService.uploadPic(pic.getBytes(), pic.getOriginalFilename(), pic.getSize());
-//		
-//		String url = Constants.IMG_URL + path;
-//		
-//		JSONObject  jo = new JSONObject();
-//		jo.put("url", url);
-//		 
-//		response.setContentType("application/json;charset=UTF-8");
-//		response.getWriter().write(jo.toString());
-		System.out.println(pic.getOriginalFilename()+"<<<<<<<<<");
+		String path = uploadService.uploadPic(pic.getBytes(), pic.getOriginalFilename(), pic.getSize());
+		
+		String url = Constants.IMG_URL + path;
+		
+		JSONObject  jo = new JSONObject();
+		jo.put("url", url);
+		 
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(jo.toString());
 	}
 }
--- oliver-service-product/src/main/java/cn/itcast/core/service/product/UploadServiceImpl.java
+++ oliver-service-product/src/main/java/cn/itcast/core/service/product/UploadServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.itcast.core.service.product;
 
 import org.springframework.stereotype.Service;
+import cn.itcast.common.fdfs.FastDFSUtils;
 
 
 @Service("uploadService")
@@ -9,7 +10,6 @@ public class UploadServiceImpl implements UploadService{
 	
 	//上传图片
 	public String uploadPic(byte[] pic ,String name,long size){
-		//return FastDFSUtils.uploadPic(pic, name, size);
-		return "";
+		return FastDFSUtils.uploadPic(pic, name, size);
 	}
 }
